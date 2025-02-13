eebd09475136d49d161b3ff7dae88054fe9cc249
rozzy <rosekaremeri@gmail.com>
2025-02-13 16:10:49 +0300

    feture: define routes

---
+++ crud_backend/src/routes/clientRoute.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+import * as clientController from '../controllers/clientController' 
+
+const router = express.Router();
+
+router.get('/clients', clientController.getClients)
