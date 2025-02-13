a58ae08a714a0fd1ef9d247cf3b0ae4d9932457b
rozzy <rosekaremeri@gmail.com>
2025-02-13 10:59:04 +0300

    Feature: configure server and db

--- db.js
+++ db.js
@@ -3,5 +3,8 @@ const pool = new Pool({
     host: 'db',
     port: 5432,
     user: 'user123',
-    password: 'password123'
+    password: 'password123',
+    database: 'db123'
 })
+
+module.exports = pool
