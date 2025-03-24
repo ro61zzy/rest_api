68b2557b2ac0bbbe219653fa2d3eb179d658d219
rozzy <rosekaremeri@gmail.com>
2025-03-24 06:17:09 +0300

    basic api structure

---
+++ node_rest_api/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+const port = 4000;
+
+let movies = [
+  {
+    id: "1",
+    title: "Inception",
+    director: "Christopher Nolan",
+    release_date: "2010-07-16",
+  },
+  {
+    id: "1",
+    title: "The Irishman",
+    director: "Martin Scorsese",
+    release_date: "2019-09-27",
+  },
+];
+
+
+//get 
+
+//post
